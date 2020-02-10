72c22786adebb67e8e667b873d8ed77262fd7d40
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-10 21:35:50 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -1,26 +1,26 @@
-?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
 	<channel>
-		<title>All Top News -- ScienceDaily</title>
-		<link>https://www.sciencedaily.com/news/top/</link>
-		<description>Top science stories featured on ScienceDaily's home page.</description>
+		<title>Top Technology News -- ScienceDaily</title>
+		<link>https://www.sciencedaily.com/news/top/technology/</link>
+		<description>Top stories featured on ScienceDaily's Space &amp; Time, Matter &amp; Energy, and Computers &amp; Math sections.</description>
 		<language>en-us</language>
-		<pubDate>Mon, 10 Feb 2020 07:33:02 EST</pubDate>
-		<lastBuildDate>Mon, 10 Feb 2020 07:33:02 EST</lastBuildDate>
+		<pubDate>Mon, 10 Feb 2020 07:53:03 EST</pubDate>
+		<lastBuildDate>Mon, 10 Feb 2020 07:53:03 EST</lastBuildDate>
 		<ttl>60</ttl>
 		<image>
-			<title>All Top News -- ScienceDaily</title>
+			<title>Top Technology News -- ScienceDaily</title>
 			<url>https://www.sciencedaily.com/images/scidaily-logo-rss.png</url>
-			<link>https://www.sciencedaily.com/news/top/</link>
+			<link>https://www.sciencedaily.com/news/top/technology/</link>
 			<description>For more science news, visit ScienceDaily.</description>
 		</image>
-		<atom:link xmlns:atom="http://www.w3.org/2005/Atom" rel="self" href="https://www.sciencedaily.com/rss/top.xml" type="application/rss+xml" />
+		<atom:link xmlns:atom="http://www.w3.org/2005/Atom" rel="self" href="https://www.sciencedaily.com/rss/top/technology.xml" type="application/rss+xml" />
 		<item>
-			<title>One small grain of moon dust, one giant leap for lunar studies</title>
-			<link>https://www.sciencedaily.com/releases/2020/02/200207095427.htm</link>
-			<description>Scientists have found a new way to analyze the chemistry of the moon&#039;s soil using a single grain of dust brought back by Apollo 17 astronauts in 1972. Their technique can help us learn more about conditions on the surface of the moon and formation of precious resources like water and helium there. <!-- more --></description>
-			<pubDate>Fri, 07 Feb 2020 09:54:27 EST</pubDate>
-			<guid isPermaLink="true">https://www.sciencedaily.com/releases/2020/02/200207095427.htm</guid>
+			<title>First-of-its-kind hydrogel platform enables on-demand production of medicines, chemicals</title>
+			<link>https://www.sciencedaily.com/releases/2020/02/200209155505.htm</link>
+			<description>A team of chemical engineers has developed a new way to produce medicines and chemicals on demand and preserve them using portable &#039;&#039;biofactories&#039;&#039; embedded in water-based gels called hydrogels. The approach could help people in remote villages or on military missions, where the absence of pharmacies, doctor&#039;s offices or even basic refrigeration makes it hard to access critical medicines, daily use chemicals and other small-molecule compounds. <!-- more --></description>
+			<pubDate>Sun, 09 Feb 2020 15:55:05 EST</pubDate>
+			<guid isPermaLink="true">https://www.sciencedaily.com/releases/2020/02/200209155505.htm</guid>
 		</item>
 	</channel>
 </rss>
