8c2e6a4597191af89db937069346ddfffe2f57b5
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-11 00:49:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,58 @@
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+ 
+  h1{
+ text-align: center;
+ color: black;
+ }
+ .container{
+  height: auto;
+  width: auto;
+  padding: 0;
+  margin: 0;
+ }
+ .container li{
+ color: white;
+ }
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="index.php">SCIENCE NEWS</a></li>
+  <li><a href="tech.php">TECHNOLOGY NEWS</a></li>
+  <li><a class="active" href="health.php">HEALTH NEWS</a></li>
+  <li><a href="envi.php">ENVIRONMENT NEWS</a></li>
+</ul>
+
+</body>
+</html>
+
+
 <?php
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://www.sciencedaily.com/rss/top.xml"); 
