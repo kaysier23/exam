d70111f688a64e28b9aefde04104249d202cdd0d
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-10 21:37:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
  $domOBJ = new DOMDocument();
- $domOBJ->load("https://www.sciencedaily.com/rss/top/science.xml"); 
+ $domOBJ->load("https://www.sciencedaily.com/rss/top/technology.xml"); 
  
  $content = $domOBJ->getElementsByTagName("item");
  
