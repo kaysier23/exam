fa3181f1bf12ddd590022a28e1c33dd96e5b3d3b
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-10 22:04:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
    $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
    $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
    $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
-  <container> 
   echo "
    <ul>
             <h2><a href = '$link'> $title </a></h2>
@@ -29,7 +28,6 @@
                   </ul>
     </ul>
             ";
-  <container>
   ?>
 </body>
 <?php
