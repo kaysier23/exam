07c39b4736dc02f70675660d9fa1bfa0ac2cdeed
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-12 19:08:50 +0800

    Update tech.php

--- tech.php
+++ tech.php
@@ -46,8 +46,42 @@ li a:hover {
 <ul>
   <li><a class="active" href="index.php">TOP NEWS</a></li>
   <li><a href="sci.php">SCIENCE NEWS</a></li>
+  <li><a href="tech.php">TECHNOLOGY NEWS</a></li>
 </ul>
 
 </body>
 </html>
 
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://www.sciencedaily.com/rss/top/technology.xml");  
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+ 
+ ?>
+  <h1>All Top News -- ScienceDaily</h1>
+
+    <?php
+ foreach( $content as $data )
+ { ?>
+ 
+
+
+<?php
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+   $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
+   echo "
+   <ul>
+            <h2><a href = '$link'> $title </a></h2>
+              <ul>
+              <li>$description</li>
+              <li>$pubDate</li>
+                  </ul>
+    </ul>
+            ";
+  ?>
+<?php
+ }
+?>
