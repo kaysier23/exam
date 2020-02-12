04a0e36e6133a14f21171b92c5522ed74ca4335f
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-12 18:53:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+  color: white;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+ 
+  h1{
+ text-align: center;
+ color: black;
+ }
+ .container{
+  height: auto;
+  width: auto;
+  padding: 0;
+  margin: 0;
+ }
+ .container li{
+ color: white;
+ }
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="index.php">TOP NEWS</a></li>
+  <li><a href="bnews.php">TECHNOLOGY NEWS</a></li>
+  <li><a class="enews" href="health.php">HEALTH NEWS</a></li>
+  <li><a href="envi.php">ENVIRONMENT NEWS</a></li>
+</ul>
+
+</body>
+</html>
+
+
 <?php
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://rss.nytimes.com/services/xml/rss/nyt/Science.xml");  
